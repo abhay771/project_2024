2040ceb6097035aa21bdb967c416650bec8fec71
abhay771 <abhaylode11@gmail.com>
2024-08-12 21:27:53 +0530

    First commit

---
+++ src/main/java/com/project2024/proapp/ProappApplication.java
@@ -0,0 +1,13 @@
+package com.project2024.proapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProappApplication.class, args);
+	}
+
+}
