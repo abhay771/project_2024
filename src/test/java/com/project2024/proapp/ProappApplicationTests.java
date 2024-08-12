88bd5734d60f5fed2d32a4a602ad9017721e4023
abhay771 <abhaylode11@gmail.com>
2024-08-12 21:50:05 +0530

    push

---
+++ src/test/java/com/project2024/proapp/ProappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project2024.proapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
